3ff39f033ac4defb90974641c57f920e5827b442
Abigail Harrison <abigail@giffgaff.co.uk>
2022-10-21 17:42:22 +0100

    #33 wip: Fix hydration issue

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,52 @@
-import Image from 'next/image';
-import Layout from '@/components/layout';
-import Button from '@/components/button';
-import Card from '@/components/card';
-import Table from '@/components/table';
+import Image from 'next/image'
+import Layout from '@/components/layout'
+import Button from '@/components/button'
+import Card from '@/components/card'
+import Table from '@/components/table'
 
 export default function Home() {
-	return (
-		<>
-			<Layout>
-				<h1>Welcome to The First Drop!</h1>
+  return (
+    <>
+      <Layout>
+        <h1>Welcome to The First Drop!</h1>
 
         <p>Test homepage</p>
-				<Button>Test button</Button>
-				<Card>
-					<p>Test card</p>
-				</Card>
-				<Table>
-					<thead>
-						<th>Test header</th>
-						<th>Test header</th>
-						<th>Test header</th>
-					</thead>
-					<tbody>
-						<tr>
-							<td>Test description</td>
-							<td>Test description</td>
-							<td>Test description</td>
-						</tr>
-						<tr>
-							<td>Test description</td>
-							<td>Test description</td>
-							<td>Test description</td>
-						</tr>
-						<tr>
-							<td>Test description</td>
-							<td>Test description</td>
-							<td>Test description</td>
-						</tr>
-						<tr>
-							<td>Test description</td>
-							<td>Test description</td>
-							<td>Test description</td>
-						</tr>
-					</tbody>
-				</Table>
-			</Layout>
-		</>
-	);
+        <Button>Test button</Button>
+        <Card>
+          <p>Test card</p>
+        </Card>
+        <Table>
+          <thead>
+            <tr>
+              <th>Test header</th>
+              <th>Test header</th>
+              <th>Test header</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Test description</td>
+              <td>Test description</td>
+              <td>Test description</td>
+            </tr>
+            <tr>
+              <td>Test description</td>
+              <td>Test description</td>
+              <td>Test description</td>
+            </tr>
+            <tr>
+              <td>Test description</td>
+              <td>Test description</td>
+              <td>Test description</td>
+            </tr>
+            <tr>
+              <td>Test description</td>
+              <td>Test description</td>
+              <td>Test description</td>
+            </tr>
+          </tbody>
+        </Table>
+      </Layout>
+    </>
+  )
 }
