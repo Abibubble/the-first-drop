ea43baa53c52d792adff82c9a3caa65b27d8eff8
Abigail Harrison <abigail@giffgaff.co.uk>
2022-10-21 16:53:05 +0100

    wip: A11y fixes

--- components/button.js
+++ components/button.js
@@ -1,12 +1,7 @@
 import React, { ReactNode } from 'react'
-import styles from '@/styles/Button.module.css';
+import styles from '@/styles/Button.module.css'
 
-export default function Button({
-  href,
-  onClick,
-  ariaLabel,
-  ...props
-}) {
+export default function Button({ href, onClick, ariaLabel, ...props }) {
   let elementType
   if (href) {
     elementType = 'a'
@@ -18,13 +13,11 @@ export default function Button({
       {...(href && { href: href })}
       target={href ? '_blank' : '_self'}
       rel={href ? 'noopener noreferrer' : ''}
-      aria-label={ariaLabel}
+      aria-label={ariaLabel ? ariaLabel : 'Button'}
       onClick={onClick}
       className={styles.main}
     >
-      <span>
-        {props.children}
-      </span>
+      <span>{props.children}</span>
     </button>
   )
 }
