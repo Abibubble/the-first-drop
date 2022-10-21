e0c6466d937c00547c57e02cd5476541b8a91447
Abigail Harrison <abigail@giffgaff.co.uk>
2022-10-21 18:57:13 +0100

    #35 ref: Refactor pa11y script

--- scripts/pa11y.js
+++ scripts/pa11y.js
@@ -6,7 +6,6 @@ const packageJson = require(appRoot + '/package.json')
 const colors = require('colors/safe')
 let data
 
-// Checks if a config file exists, and if it does, add the path to it to config variable.
 try {
   data = packageJson.accessibility
 } catch (err) {
@@ -17,10 +16,9 @@ async function runPa11y(url) {
   try {
     const result = await pa11y(url)
 
-    // Print out message to user with celebratory message
-    console.log(colors.yellow('A11y summary for ' + url))
+    console.log(colors.yellow(`A11y summary for ${url}`))
 
-    if (result.issues.length == 0) {
+    if (!result.issues.length) {
       console.log(
         colors.green(`
 ====================================================================================
@@ -33,22 +31,24 @@ async function runPa11y(url) {
     } else {
       console.log(
         colors.yellow(
-          'There are ' + result.issues.length + ' issues with this page:'
+          `There ${result.issues.length == 1 ? 'is' : 'are'} ${
+            result.issues.length
+          } issue${result.issues.length == 1 ? '' : 's'} with this page:`
         )
       )
     }
 
-    // Output the raw result object
-    for (let i = 0; i < result.issues.length; i++) {
-      console.log(colors.red('A11Y ERROR! ' + result.issues[i].message))
+    result.issues.forEach(issue => {
+      console.log(colors.red(`A11Y ERROR! ${issue.message}`))
+      console.log(colors.brightCyan(`The offending code: ${issue.context}`))
       console.log(
-        colors.brightCyan('The offending code: ' + result.issues[i].context)
+        colors.brightMagenta(
+          '           ================================================================          '
+        )
       )
-      console.log('===========================================================')
-    }
-  } catch (error) {
-    // Output an error if it occurred
-    console.error(colors.red(error.message))
+    })
+  } catch (e) {
+    console.error(colors.red(e.message))
   }
 }
 
